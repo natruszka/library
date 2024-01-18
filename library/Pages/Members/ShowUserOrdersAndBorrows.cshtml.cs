3ef62cd058ab0ffe75fae84311a1809e9489e867
natruszka <ola.michalska2002@gmail.com>
2024-01-18 22:27:30 +0100

    Fix userpanel bug

--- library/Pages/Members/ShowUserOrdersAndBorrows.cshtml.cs
+++ library/Pages/Members/ShowUserOrdersAndBorrows.cshtml.cs
@@ -1,24 +1,31 @@
 ﻿using library.DTOs;
+using library.Entities;
+using library.Services;
 using library.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace library.Pages.Members;
 
+[Authorize]
 public class ShowUserOrdersAndBorrows : PageModel
 {
     public List<UserBorrowView> UserBorrowViews { get; set; } = new();
     public List<UserOrderView> UserOrderViews { get; set; } = new();
+    public Member MemberToView { get; set; } = new();
     private readonly IBorrowService _borrowService;
     private readonly IOrderService _orderService;
-
-    public ShowUserOrdersAndBorrows(IBorrowService borrowService, IOrderService orderService)
+    private readonly MemberService _memberService;
+    public ShowUserOrdersAndBorrows(IBorrowService borrowService, IOrderService orderService, MemberService memberService)
     {
         _borrowService = borrowService;
         _orderService = orderService;
+        _memberService = memberService;
     }
     public void OnGet(int userId)
     {
         UserBorrowViews = new List<UserBorrowView>(_borrowService.GetUsersBorrow(userId));
         UserOrderViews = new List<UserOrderView>(_orderService.GetUsersOrder(userId));
+        MemberToView = _memberService.GetMemberById(userId)!;
     }
 }
